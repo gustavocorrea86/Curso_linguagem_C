35b4b3cd2abe52206bb8154e5a83aaa9b69ca651
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-09 17:24:03 -0300

    codeblocks

--- aula1.c
+++
@@ -1,8 +0,0 @@
-int main(int argc, char const *argv[])
-{
-     char nome;
-    printf("Qual seu nome?");
-    scanf("%s",&nome);
-    printf("Muito prazer, %s",&nome);
-    
-}
---
+++ aula1/a01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//#include <stdlib.h>
+void main(){
+//printf("Ola mundo");
+char nome[10];
+printf("Qual seu nome: \b");
+scanf("%s", &nome);
+printf("Muito prazer %s\n", nome);
+//system("pause")
+}
