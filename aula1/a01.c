e4077ddf834b5cf0c88e9f0e1f938ec3f3645c13
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-09 18:03:36 -0300

    atualizando

--- aula1/a01.c
+++ aula1/a01.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 void main(){
-//printf("Ola mundo");
-char nome[10];
-printf("Qual seu nome: \b");
-scanf("%s", &nome);
-printf("Muito prazer %s\n", nome);
-//system("pause")
+    //printf("Ola mundo");
+    char nome[10];
+    printf("Qual seu nome: \b");
+    scanf("%s", &nome);
+    printf("Muito prazer %s\n", nome);
+    system("pause");
 }
